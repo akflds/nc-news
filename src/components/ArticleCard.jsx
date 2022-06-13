90fd6e0ed0b6c17e3ed8662257312ac6544f7f8b
akflds <andrewkirsfelds@gmail.com>
2022-06-13 16:20:11 +0100

    initial styling for articles component

--- src/components/ArticleCard.jsx
+++ src/components/ArticleCard.jsx
@@ -4,11 +4,16 @@ const ArticleCard = ({ title, author, topic, votes, comment_count }) => {
   return (
     <article className={styles.articleCard}>
       <h3>{title}</h3>
-      <p>Author: {author}</p>
-      <p>Posted in {topic}</p>
-      <p>
-        Votes: {votes}. Comments: {comment_count}
-      </p>
+      <div className={styles.articleInfo}>
+        <p>Author: {author}</p>
+        <p>
+          Posted under <span className={styles.topic}>{topic}</span>
+        </p>
+      </div>
+      <div className={styles.articleStats}>
+        <p>Votes: {votes}</p>
+        <p>Comments: {comment_count}</p>
+      </div>
     </article>
   );
 };
