a97c84b71ff30f57881398c9b4bee87f9194326f
akflds <andrewkirsfelds@gmail.com>
2022-06-14 08:33:06 +0100

    add topic placeholder images and link to main page

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,9 +1,12 @@
+import { Link } from "react-router-dom";
 import styles from "./Header.module.css";
 
 const Header = () => {
   return (
     <header className={styles.header}>
-      <h1>NC News</h1>
+      <Link to="/">
+        <h1>NC News</h1>
+      </Link>
     </header>
   );
 };
