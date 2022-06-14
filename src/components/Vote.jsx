6db7a86d0bd12a6ef835b8c2eeabfa6bed32d89d
akflds <andrewkirsfelds@gmail.com>
2022-06-14 16:13:11 +0100

    fix voting to only allow a single vote

--- src/components/Vote.jsx
+++ src/components/Vote.jsx
@@ -1,8 +1,15 @@
 import styles from "./Vote.module.css";
 import { updateVote } from "../api/api";
+import { useState } from "react";
 
 const Vote = ({ article_id, setVotes }) => {
+  const [voted, setVoted] = useState(false);
+  const [voteDiff, setVoteDiff] = useState(0);
+
   const handleClick = (amount) => {
+    setVoted((curr) => !curr);
+    setVoteDiff((curr) => (curr += amount));
+
     setVotes((curr) => (curr += amount));
     updateVote(article_id, amount).catch((error) => {
       setVotes((curr) => (curr -= amount));
@@ -12,15 +19,19 @@ const Vote = ({ article_id, setVotes }) => {
   return (
     <div className={styles.voteContainer}>
       <button
+        className={voted && voteDiff > 0 ? `${styles.clicked}` : null}
+        disabled={voted && voteDiff < 0}
         onClick={() => {
-          handleClick(1);
+          voted ? handleClick(-1) : handleClick(1);
         }}
       >
         Increase
       </button>
       <button
+        className={voted & (voteDiff < 0) ? `${styles.clicked}` : null}
+        disabled={voted && voteDiff > 0}
         onClick={() => {
-          handleClick(-1);
+          voted ? handleClick(1) : handleClick(-1);
         }}
       >
         Decrease
