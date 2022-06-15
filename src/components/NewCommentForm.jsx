5ef0a2d7d2544af9069e2c47d5d0bc256e120c2e
akflds <andrewkirsfelds@gmail.com>
2022-06-15 14:57:55 +0100

    setup form basics

--- src/components/NewCommentForm.jsx
+++ src/components/NewCommentForm.jsx
@@ -1,8 +1,28 @@
+import { useState } from "react";
 import { useParams } from "react-router-dom";
 
 const NewCommentForm = () => {
+  const [newComment, setNewComment] = useState("");
   const { article_id } = useParams();
-  return <p>Formy McFormface</p>;
+
+  const handleSubmit = (event) => {
+    event.preventDefault();
+    console.log(newComment);
+    // todo api call
+  };
+  return (
+    <form onSubmit={handleSubmit}>
+      <label>
+        Comment:
+        <textarea
+          value={newComment}
+          onChange={(e) => setNewComment(e.target.value)}
+          placeholder="Tell me what you're thinking..."
+        ></textarea>
+      </label>
+      <input type="submit" value="Submit" />
+    </form>
+  );
 };
 
 export default NewCommentForm;
