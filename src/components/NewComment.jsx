822afa8d2a22e51721fab97c523032be19ebf77c
akflds <andrewkirsfelds@gmail.com>
2022-06-15 14:34:43 +0100

    toggle button to display new comment

--- src/components/NewComment.jsx
+++ src/components/NewComment.jsx
@@ -1,7 +1,21 @@
+import { useState } from "react";
 import styles from "./NewComment.module.css";
 
 const NewComment = () => {
-  return <h3>New comment goes here</h3>;
+  const [showPostCommentForm, setShowPostCommentForm] = useState(false);
+  return (
+    <section>
+      <button
+        className={`${styles.showPostCommentFormButton} ${
+          showPostCommentForm ? `${styles.clicked}` : ""
+        }`}
+        onClick={() => setShowPostCommentForm((curr) => !curr)}
+      >
+        {showPostCommentForm ? "Hide new comment" : "New comment"}
+      </button>
+      {showPostCommentForm ? <h3>Comments go here</h3> : null}
+    </section>
+  );
 };
 
 export default NewComment;
