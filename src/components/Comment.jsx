6f518cf325f5f01c4c6c9056874db01d732c950c
akflds <andrewkirsfelds@gmail.com>
2022-06-16 14:11:09 +0100

    delete confirmation

--- src/components/Comment.jsx
+++ src/components/Comment.jsx
@@ -1,11 +1,34 @@
 import styles from "./Comment.module.css";
 import { UserContext } from "../contexts/User";
-import { useContext } from "react";
+import { useContext, useState } from "react";
+import { deleteComment } from "../api/api";
 
 import Vote from "./Vote";
 
-const Comment = ({ comment_id, author, body, votes }) => {
+const Comment = ({ comment_id, author, body, votes, setComments }) => {
   const { user } = useContext(UserContext);
+  const [isDeleting, setIsDeleting] = useState(false);
+  const [deleted, setDeleted] = useState(false);
+
+  const handleDelete = () => {
+    setIsDeleting(true);
+    deleteComment(comment_id)
+      .then(() => {
+        setDeleted(true);
+      })
+      .catch((error) => {
+        setIsDeleting(false);
+      });
+  };
+
+  if (deleted) {
+    setTimeout(() => {
+      setComments((curr) => {
+        return curr.filter((comment) => comment.comment_id !== comment_id);
+      });
+    }, 1200);
+    return <p className={styles.deleted}>Comment deleted!</p>;
+  }
 
   return (
     <li className={styles.commentContainer}>
@@ -13,7 +36,15 @@ const Comment = ({ comment_id, author, body, votes }) => {
         <p>
           <span className={styles.author}>{author}</span> posted:
         </p>
-        {user.name === author ? <button>Delete</button> : null}
+        {user.name === author ? (
+          <button
+            disabled={isDeleting}
+            className={styles.deleteCommentButton}
+            onClick={handleDelete}
+          >
+            {isDeleting ? "Deleting..." : "Delete"}
+          </button>
+        ) : null}
       </div>
 
       <p>{body}</p>
