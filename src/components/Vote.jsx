bc1e2d5a21b1f6a0dbe1f76276368199cd0536b3
akflds <andrewkirsfelds@gmail.com>
2022-06-17 11:58:08 +0100

    remove duplicate state to track voted status

--- src/components/Vote.jsx
+++ src/components/Vote.jsx
@@ -8,13 +8,11 @@ const Vote = ({ article_id, comment_id, votes, author }) => {
   const { user } = useContext(UserContext);
 
   const [currentVotes, setCurrentVotes] = useState(votes);
-  const [voted, setVoted] = useState(false);
   const [voteDiff, setVoteDiff] = useState(0);
 
   // TODO: track if a user has voted on an article or comment already
 
   const handleClick = (amount) => {
-    setVoted((curr) => !curr);
     setVoteDiff((curr) => (curr += amount));
 
     setCurrentVotes((curr) => (curr += amount));
@@ -24,32 +22,30 @@ const Vote = ({ article_id, comment_id, votes, author }) => {
       amount
     ).catch((error) => {
       setCurrentVotes((curr) => (curr -= amount));
-      setVoted((curr) => !curr);
       setVoteDiff((curr) => (curr -= amount));
     });
   };
 
   // TODO: button code feels quite WET, consider refactor into VoteButton
-  // TODO: check if voted is necessary or can be done entirely on voteDiff
 
   return (
     <div className={styles.voteContainer}>
       <p>Votes: {currentVotes}</p>
       <div className={styles.voteButtonContainer}>
         <button
-          className={voted && voteDiff > 0 ? `${styles.clicked}` : null}
-          disabled={(voted && voteDiff < 0) || user.name === author}
+          className={voteDiff > 0 ? `${styles.clicked}` : null}
+          disabled={voteDiff < 0 || user.name === author}
           onClick={() => {
-            voted ? handleClick(-1) : handleClick(1);
+            voteDiff > 0 ? handleClick(-1) : handleClick(1);
           }}
         >
           Upvote
         </button>
         <button
-          className={voted & (voteDiff < 0) ? `${styles.clicked}` : null}
-          disabled={(voted && voteDiff > 0) || user.name === author}
+          className={voteDiff < 0 ? `${styles.clicked}` : null}
+          disabled={voteDiff > 0 || user.name === author}
           onClick={() => {
-            voted ? handleClick(1) : handleClick(-1);
+            voteDiff < 0 ? handleClick(1) : handleClick(-1);
           }}
         >
           Downvote
